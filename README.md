15fac32f5548f011bcc99c0a25d4964b18c45ed2
Joshua Smith <jsmith@stemwinderproductions.com>
2015-03-01 16:05:29 -0500

    support for OS X, changes config file name
    Also brings additions to readme file

--- README.md
+++ README.md
@@ -1,7 +1,26 @@
 # lftp-sync
-A simple interface for using lftp to mirror remote data structures
+A simple interface for using lftp to mirror remote data structures based on file modification times.
+
+## Setup
+* Download or clone the repository to the instllation path of choice on the target machine.
+* Copy `lftp-sync-defaults.cfg` to `lftp-sync.cfg` and add/change parameters
+* Ensure the user issuing lftp-sync commands has write permissions to the installation and `lftp-output` directories
+
+## Usage
+Basic usage:
+    ./lftp-sync.sh -s "/remote/source/path/" -t "/path/to/target"
+With verbose logging, override date, and download limit:
+    ./lftp-sync.sh -s "/remote/source/path/" -t "/path/to/target" -v 3 -o "2015-02-20" -d 5M
+
+## Caveats
+* The `lftp-sync.sh` command must currently be issued from its parent directory.
+    * This is due to change
+* All dates are converted to UTC before being used for input or output.
+* Usage on OS X requires `greadlink` and `gdate` provided by Homebrew `coreutils`.
+* Overriding the time spec from the command line will not produce an entry in `lftp-timestamps.log`.
 
 ## To Do's
+* Detect OS X and use `gdate` and `greadlink` instead
 * Ability to run from any path location
 ** Currently must be run from parent directory
 * Ability to specify logfile locations as arguments
--- lftp-sync.sh
+++ lftp-sync.sh
@@ -1,33 +1,54 @@
 #!/bin/bash
 
+# support for `coreutils` on OS X
+if [[ $OSTYPE == darwin* ]]; then
+    readlink_cmd='greadlink'
+    date_cmd='gdate'
+else
+    readlink_cmd='readlink'
+    date_cmd='date'
+fi
+
+# set path and name variables
+this_path=$("$readlink_cmd" -f $0)  ## Path of this file including filename
+dir_name=`dirname ${this_path}`     ## Dir where this file is
+myname=`basename ${this_path}`      ## file name of this script.
+
+echo $this_path; echo $dir_name; echo $myname; exit 1
+
 # inlcude default config settings
-source ./lftp-sync-defaults.cfg
+if [ ! -f "./lftp-sync.cfg" ] ; then
+    echo "No config file was found. Exiting."
+    exit 1
+else
+    source ./lftp-sync.cfg
+fi
 
 # define and display help info to the user
 function help {
   echo "
   usage: lftp-sync [options]
-  -h           optional  Print this help message
-  -s           required  Path to remote source.
-               Adding or removing a trailing slash will affect the behaviour.
-  -t           required  Path to local target.
-               Adding or removing a trailing slash will affect the behaviour.
-  -v           optional  Lftp mirror verbosity level
-               default is 0, options are 1, 2 and 3
-  -m           optional  Number of parallel downloads.
-               Smaller files will benefit from more concurrent downloads.
-               default is 15
-  -n           optional  Number of pget segments per download.
-               Larger files will benefit from higher segment counts.
-               default is 10
-  -u           optional  Total Upload limit
-               default is 0 (unlimited)
-               This argument is passed directly to lftp
-  -d           optional  Total Download limit
-               default is 0 (unlimited)
-               This argument is passed directly to lftp
-  -o           optional  Time override (--newer-than)
-               Overrides the default time behaviour of script"
+  -h        optional  Print this help message
+  -s        required  Path to remote source.
+            Adding or removing a trailing slash will affect the behaviour.
+  -t        required  Path to local target.
+            Adding or removing a trailing slash will affect the behaviour.
+  -v        optional  Lftp mirror verbosity level
+            default is 0, options are 1, 2 and 3
+  -m        optional  Number of parallel downloads.
+            Smaller files will benefit from more concurrent downloads.
+            default is 15
+  -n        optional  Number of pget segments per download.
+            Larger files will benefit from higher segment counts.
+            default is 10
+  -u        optional  Total Upload limit
+            default is 0 (unlimited)
+            This argument is passed directly to lftp
+  -d        optional  Total Download limit
+            default is 0 (unlimited)
+            This argument is passed directly to lftp
+  -o        optional  Time override (--newer-than)
+            Overrides the default time behaviour of script"
   exit 1
 }
 
@@ -39,7 +60,6 @@ function log {
 # If no arguments are passed to this script, it should always show the usage info.
 # The script will never perform any action unless the necessary parameters are explicitly used.
 if [ $# == 0 ] ; then
-  # Show the help usage
   help
   exit 1;
 fi
@@ -77,6 +97,7 @@ while getopts s:t:v:m:n:h:u:d:o: opt; do
   esac
 done
 
+# makes paramters accesible by arguement number (eg: $1, $2, ...)
 shift $((OPTIND - 1))
 
 # making sure we have both a source and target path
@@ -104,7 +125,7 @@ done < "$time_log_file"
 # check if we got a timestamp from logfile,
 # if not, set TIMESPEC to current datetime in UTC
 if [[ -z "$TIME_FROM_LOG" ]]; then
-  TIMESPEC="$(date -u +"%Y-%m-%d %H:%M:%S")"
+  TIMESPEC="$("$date_cmd" -u +"%Y-%m-%d %H:%M:%S")"
   log "TIMESPEC was set from system time"
 else
   TIMESPEC="$TIME_FROM_LOG"
@@ -113,18 +134,18 @@ fi
 
 # check for timespec override from command line
 if ! [[ -z "$newer_than" ]]; then
-  TIMESPEC="$(date -d "$newer_than" -u +"%Y-%m-%d %H:%M:%S")"
+  TIMESPEC="$("$date_cmd" -d "$newer_than" -u +"%Y-%m-%d %H:%M:%S")"
   log "TIMESPEC was overriden from command line ($TIMESPEC)"
 else
   # write current time to logfile in UTC if timespec wasn't overriden
-  echo "$(date -u +"%Y-%m-%d %H:%M:%S")" >> "$time_log_file"
+  echo "$("$date_cmd" -u +"%Y-%m-%d %H:%M:%S")" >> "$time_log_file"
 fi
 
 # execute lftp command
 log "Start lftp sync"
 lftp_command="lftp -c \"connect -u $username,$password sftp://$server:$port; set net:limit-total-rate $dl_limit:$ul_limit; mirror -p --verbose=$verbosity --no-empty-dirs --newer-than=\\\"$TIMESPEC\\\" --parallel=$streams --use-pget-n=$segments \\\"$source_path\\\" \\\"$target_path\\\"; quit\""
 log $lftp_command
-eval $lftp_command 2>&1 | tee "$lftp_logout_dir$(date -u +"%Y-%m-%d_%H:%M:%S").log"
+eval $lftp_command 2>&1 | tee "$lftp_log_dir$("$date_cmd" +"%Y-%m-%d_%H:%M:%S_%Z").log"
 
 # exit script with success code
 log "Script execution complete"
